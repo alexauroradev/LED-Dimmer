5d56695bedc7f36e6e8b7145f7b452635c61f113
Aleksandr Shevchenko <aleksandr.shevchenko@xdev.re>
2019-07-14 11:21:37 +0300

    Initial commit

---
+++ main.ino
@@ -0,0 +1,53 @@
+#include <EEPROM.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial BT(3, 2); // RX, TX
+
+#define LED_PIN 13 
+#define PWM_PIN 11 //initialize digital pin 11（PWM output)
+
+#define PWM_MIN 0
+#define PWM_MAX 255
+int pwm;
+String readString;
+
+void setup() {
+  pinMode(PWM_PIN,OUTPUT);// define digital pin 11 as “output”
+  Serial.begin(9600);// set baud rate at 9600
+  while (!Serial) {
+    ;
+  }
+  Serial.println("Bluetooth On please press 1 or 0 to raise or lower PWM");
+  pinMode(LED_PIN,OUTPUT);
+  BT.begin(9600);
+  pwm = PWM_MIN;
+  readString = "";
+}
+
+void loop() {
+  
+  while (BT.available()) {
+    delay(3);  //delay to allow buffer to fill 
+    if (BT.available()) {
+      char c = BT.read();  //gets one byte from serial buffer
+      readString += c; //makes the string readString
+    } 
+  }
+  
+  if (readString.length() > 0) {
+    Serial.print(readString);
+    
+    //Logic of parsing the string into commands
+    
+    if (readString.length() > 2) {
+      pwm = map(readString.substring(0,2).toInt(), 0, 99, PWM_MIN, PWM_MAX);
+    }
+    pwm = constrain(pwm, PWM_MIN, PWM_MAX);
+    analogWrite(PWM_PIN, pwm);
+  }
+  delay(100);
+  readString = "";
+}
+
+/////////////////////////////////////////////////////
